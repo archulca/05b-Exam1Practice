f8026e387bc31ddd2647356e30620e49ceb460ca
Carla Archuleta <archulca@rose-hulman.edu>
2019-01-31 21:58:58 -0500

    TODO #6

--- src/problem0.py
+++ src/problem0.py
@@ -266,7 +266,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ###########################################################################
@@ -275,7 +275,11 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ###########################################################################
     # ------------------------------------------------------------------
-
+    count = 0
+    for k in range(2,n+1):
+        if is_prime(k):
+            count = count + 1
+    return count
 def run_test_problem0c():
     """ Tests the   problem0c  function. """
     print()
